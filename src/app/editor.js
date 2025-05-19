7ed10b15873cf5c4ee7a837031da625d06c56c0e
m-royer <prince.snoopy@gmail.com>
2025-05-19 15:16:15 -0700

    App Alpha

---
+++ src/app/editor.js
@@ -0,0 +1,16 @@
+
+
+export function Editor() {
+    return (
+        <div className="editor font-patrick text-sky-950 text-xl h-full mt-2" contentEditable="true">
+            <p>[Date]</p>
+            <p>&nbsp;</p>
+            <p>Dear Hiring Manager,</p>
+            <p>&nbsp;</p>
+            <p>Sample Text.</p>
+            <p>&nbsp;</p>
+            <p>Thank you,</p>
+            <p>Name</p>
+        </div>
+    );
+}
